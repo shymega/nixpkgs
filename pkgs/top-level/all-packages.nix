905acfb36215ddc318bc141643dbfa654330f7a3
Louis Tim Larsen <louis@louis.dk>
2020-08-15 19:23:36 +0200

    [wip] crossover: init at 19.02
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29803,6 +29803,8 @@ with pkgs;
 
   crun = callPackage ../applications/virtualization/crun { };
 
+  crossover = callPackage ../applications/misc/crossover {};
+
   csdp = callPackage ../applications/science/math/csdp { };
 
   csv2svg = callPackage ../tools/graphics/csv2svg { };
