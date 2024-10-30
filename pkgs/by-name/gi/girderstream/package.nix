485ec49377108b19322b6c7164905e60307daaf6
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-30 21:40:43 +0000

    girderstream: init at v0.1.0

---
+++ pkgs/by-name/gi/girderstream/package.nix
@@ -0,0 +1,47 @@
+{
+  rustPlatform,
+  fetchFromGitLab,
+  lzip,
+  patch,
+  bubblewrap,
+  fuse3,
+  lib,
+  pdm,
+  buildbox,
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "girderstream";
+  version = "${pname}-v0.1.0";
+
+  src = fetchFromGitLab {
+    owner = pname;
+    repo = pname;
+    tag = version;
+    hash = "sha256-9XPNGmQfzniwzOb1oiHiUEMHF0UX73or4c4/g8wFKvA=";
+  };
+
+  cargoLock = {
+    lockFile = "${src}/Cargo.lock";
+    allowBuiltinFetchGit = true;
+  };
+
+  propagatedBuildInputs =
+    [
+      buildbox
+      fuse3
+      lzip
+      patch
+    ];
+
+  nativeBuildInputs = [
+    bubblewrap
+  ];
+
+  doCheck = false;
+
+  meta = {
+    platforms = lib.platforms.linux;
+    mainProgram = "girderstream";
+    maintainers = with lib.maintainers; [ shymega ];
+  };
+  }
